ba3792cb2ef97bdf7284b5cb3fa9607e47268b58
alex <alex@nabu.be>
2015-12-02 09:41:34 +0100

    Added server headers

--- src/main/java/be/nabu/libs/http/core/ServerHeader.java
+++ src/main/java/be/nabu/libs/http/core/ServerHeader.java
@@ -6,7 +6,9 @@ public enum ServerHeader implements CustomHeader {
 	
 	REMOTE_USER("X-Remote-User", false),
 	REMOTE_HOST("X-Remote-Host", false),
+	REMOTE_ADDRESS("X-Remote-Address", false),
 	REMOTE_PORT("X-Remote-Port", false),
+	REMOTE_IS_LOCAL("X-Remote-Is-Local", false),
 	REQUEST_URI("X-Request-URI", false),
 	RESOURCE_URI("X-Resource-URI", true),
 	REQUEST_SECURITY("X-Request-Security", false),
@@ -15,7 +17,9 @@ public enum ServerHeader implements CustomHeader {
 	
 	public static final String NAME_REMOTE_USER = "X-Remote-User";
 	public static final String NAME_REMOTE_HOST = "X-Remote-Host";
+	public static final String NAME_REMOTE_ADDRESS = "X-Remote-Address";
 	public static final String NAME_REMOTE_PORT = "X-Remote-Port";
+	public static final String NAME_REMOTE_IS_LOCAL = "X-Remote-Is-Local";
 	public static final String NAME_REQUEST_URI = "X-Request-URI";
 	public static final String NAME_RESOURCE_URI = "X-Resource-URI";
 	public static final String NAME_REQUEST_RELATIVE_URI = "X-Request-Relative-URI";
